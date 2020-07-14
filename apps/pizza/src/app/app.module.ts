9f706bc1dbeb1da212eead323dfc7b9885c7826f
Steve Witman <stevewitman@gmail.com>
2020-07-14 10:38:54 -0400

    pizza start

--- apps/pizza/src/app/app.module.ts
+++ apps/pizza/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PizzaComponent } from './pizza/pizza.component';
+import { PizzaOverviewComponent } from './pizza/pizza-overview/pizza-overview.component';
+import { PizzaDetailComponent } from './pizza/pizza-detail/pizza-detail.component';
+import { PizzaListComponent } from './pizza/pizza-list/pizza-list.component';
+import { LoginComponent } from './login/login.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, PizzaComponent, PizzaOverviewComponent, PizzaDetailComponent, PizzaListComponent, LoginComponent, FourOhFourComponent],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
